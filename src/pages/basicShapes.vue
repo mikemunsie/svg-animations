7c407690a95da5339ac5df4d22bfded82c57cf03
Mike Munsie <mike.munsie@toyotaconnected.com>
2017-09-04 21:35:45 -0500

    Animated SVG group example

--- src/pages/basicShapes.vue
+++ src/pages/basicShapes.vue
@@ -33,6 +33,17 @@
         <polygon stroke="black" fill="transparent" stroke-width=".2" points="10,4 5,13 15,13" />
       </svg>
     </div>
+
+    <div class="section">
+      <p>SVG Group Animation</p>
+      <svg viewBox="0 0 20 20" class="svg-group-animation">
+        <g class="group1">
+          <circle fill="black" stroke="gray" cx="10" cy="10" r="9" />
+          <circle fill="gray" stroke="black" cx="10" cy="10" r="7" />
+        </g>
+        <polygon class="triangle" stroke="black" fill="transparent" stroke-width=".2" points="10,4 5,13 15,13" />
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -56,6 +67,32 @@ svg {
 .svg-group {
   .group1 {
     opacity: .2;
+  }
+}
+
+.svg-group-animation .triangle {
+  animation: 2s linear 0s infinite spin;
+
+  // Determine where the center of the triangle is
+  transform-origin: 10px 10px 0;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+    fill: #fff;
+    stroke-width: .1;
+  }
+  50% {
+    transform: rotate(180deg);
+    fill: #000;
+    stroke: #fff;
+    stroke-width: .8;
+  }
+  100% {
+    transform: rotate(360deg);
+    fill: #fff;
+    stroke-width: .1;
   }
 }
 </style>
